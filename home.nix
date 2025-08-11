be6646e1c1be75557328cb6585cd23e32ba99275
shimeoki <shimeoki@gmail.com>
2025-08-12 01:10:57 +0300

    feat(nixvim): ruff

--- home.nix
+++ home.nix
@@ -254,6 +254,20 @@
       jedi_language_server = {
         enable = true;
       };
+      # fix: imports, but doesn't launch
+      ruff = {
+        enable = true;
+        settings = {
+          init_options.settings = {
+            lineLength = 80;
+            lint = { select = [ "E4" "E7" "E9" "F" ]; };
+          };
+          on_attach = ''
+            -- disable hover for ruff
+            client.server_capabilities.hoverProvider = false
+          '';
+        };
+      };
     };
   };
 
