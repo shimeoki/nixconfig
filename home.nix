0e8832710e298923bac6ace63c239a5206187d5e
shimeoki <shimeoki@gmail.com>
2025-08-16 21:48:50 +0300

    feat(git): aliases

--- home.nix
+++ home.nix
@@ -39,6 +39,22 @@
             key = "2B092E2DCA05866B";
             signByDefault = true;
         };
+        aliases = {
+            ci = "commit";
+            reword = "commit --amend";
+            st = "status --short";
+            sw = "switch";
+            new = "switch --create";
+            b = "branch";
+            del = "branch --delete";
+            list = "branch --all";
+            hist = "log --graph --oneline --all";
+            undo = "reset HEAD~";
+            unstage = "reset HEAD";
+            m = "merge";
+            ff = "merge --ff";
+            changes = "diff --cached";
+        };
     };
 
     programs.niri.settings = {
