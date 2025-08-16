c2b272d4fae75ffc03b936a3e3a0d5e21468cdc8
shimeoki <shimeoki@gmail.com>
2025-08-16 17:54:06 +0300

    style: format with nixfmt
    width = 80, indent = 4

--- configuration.nix
+++ configuration.nix
@@ -1,63 +1,77 @@
-{ config, lib, pkgs, dotfiles, nixvim, zen-browser, ... }:
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+    config,
+    lib,
+    pkgs,
+    dotfiles,
+    nixvim,
+    zen-browser,
+    ...
+}:
+{
+    imports = [
+        ./hardware-configuration.nix
+    ];
 
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.extraSpecialArgs = { inherit nixvim zen-browser; };
-  home-manager.users.d = ./home.nix;
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+    home-manager.extraSpecialArgs = { inherit nixvim zen-browser; };
+    home-manager.users.d = ./home.nix;
 
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.systemd-boot.configurationLimit = 10;
-  boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.enable = false;
+    boot.loader.systemd-boot.configurationLimit = 10;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/var/lib/sbctl";
-  };
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Europe/Moscow";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    useXkbConfig = true;
-  };
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];  
-
-  services.xserver.xkb.layout = "us,ru";
-  services.xserver.xkb.options = "grp:caps_toggle";
-
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
-  users.users.d = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-  };
-
-  services.kanata = {
-    enable = true;
-    keyboards.main = {
-      configFile = "${dotfiles}/root/private_dot_config/kanata/kanata.kbd";
+    boot.lanzaboote = {
+        enable = true;
+        pkiBundle = "/var/lib/sbctl";
     };
-  };
 
-  programs.niri.enable = true;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  environment.systemPackages = with pkgs; [
-    sbctl
-  ];
+    networking.hostName = "nixos";
+    networking.networkmanager.enable = true;
 
-  system.stateVersion = "25.05";
+    time.timeZone = "Europe/Moscow";
+
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+        font = "Lat2-Terminus16";
+        useXkbConfig = true;
+    };
+
+    nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+    ];
+
+    services.xserver.xkb.layout = "us,ru";
+    services.xserver.xkb.options = "grp:caps_toggle";
+
+    services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+    };
+
+    users.users.d = {
+        isNormalUser = true;
+        extraGroups = [
+            "wheel"
+            "networkmanager"
+        ];
+    };
+
+    services.kanata = {
+        enable = true;
+        keyboards.main = {
+            configFile = "${dotfiles}/root/private_dot_config/kanata/kanata.kbd";
+        };
+    };
+
+    programs.niri.enable = true;
+
+    environment.systemPackages = with pkgs; [
+        sbctl
+    ];
+
+    system.stateVersion = "25.05";
 }
--- flake.nix
+++ flake.nix
@@ -1,52 +1,64 @@
 {
-  description = "nixconfig";
+    description = "nixconfig";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
-      inputs.nixpkgs.follows = "nixpkgs";
+        lanzaboote = {
+            url = "github:nix-community/lanzaboote/v0.4.2";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        niri = {
+            url = "github:/sodiboo/niri-flake";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        dotfiles = {
+            url = "github:shimeoki/dotfiles";
+            flake = false;
+        };
+
+        nixvim = {
+            url = "github:nix-community/nixvim";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        zen-browser = {
+            url = "github:0xc000022070/zen-browser-flake";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    niri = {
-      url = "github:/sodiboo/niri-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    dotfiles = {
-      url = "github:shimeoki/dotfiles";
-      flake = false;
-    };
-
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { self, nixpkgs, lanzaboote, niri, home-manager, dotfiles, nixvim, zen-browser, ... }: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit dotfiles nixvim zen-browser; };
-        modules = [
-          ./configuration.nix
-          lanzaboote.nixosModules.lanzaboote
-          niri.nixosModules.niri
-	  home-manager.nixosModules.home-manager
-        ];
-      };
-    };
-  };
+    outputs =
+        {
+            self,
+            nixpkgs,
+            lanzaboote,
+            niri,
+            home-manager,
+            dotfiles,
+            nixvim,
+            zen-browser,
+            ...
+        }:
+        {
+            nixosConfigurations = {
+                nixos = nixpkgs.lib.nixosSystem {
+                    system = "x86_64-linux";
+                    specialArgs = { inherit dotfiles nixvim zen-browser; };
+                    modules = [
+                        ./configuration.nix
+                        lanzaboote.nixosModules.lanzaboote
+                        niri.nixosModules.niri
+                        home-manager.nixosModules.home-manager
+                    ];
+                };
+            };
+        };
 }
