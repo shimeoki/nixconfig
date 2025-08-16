4813c8c6f41f85bfdbaf89c76c327264d5251e60
shimeoki <shimeoki@gmail.com>
2025-08-16 21:56:35 +0300

    feat(git): other configuration

--- home.nix
+++ home.nix
@@ -39,6 +39,52 @@
             key = "2B092E2DCA05866B";
             signByDefault = true;
         };
+        extraConfig = {
+            init.defaultBranch = "main";
+            core = {
+                whitespace = "error";
+                preloadIndex = true;
+            };
+            commit.verbose = true;
+            merge = {
+                conflictStyle = "zdiff3";
+                ff = false;
+            };
+            diff = {
+                colorMoved = "default";
+                algorithm = "histogram";
+                submodule = "log";
+                renames = "copies";
+            };
+            status = {
+                branch = true;
+                showStash = true;
+                submoduleSummary = true;
+            };
+            submodule.recurse = true;
+            transfer = {
+                fsckObjects = true;
+            };
+            fetch = {
+                fsckObjects = true;
+                prune = true;
+            };
+            receive = {
+                fsckObjects = true;
+            };
+            log = {
+                date = "iso";
+                abbrevCommit = true;
+            };
+            branch.sort = "-commiterdate";
+            push = {
+                autoSetupRemote = true;
+                default = "current";
+                followTags = true;
+            };
+            pull.ff = "only";
+            tag.sort = "-taggerdate";
+        };
         aliases = {
             ci = "commit";
             reword = "commit --amend";
