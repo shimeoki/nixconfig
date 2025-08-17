b932b3777c2b4bc73ec2fd6185c25dc294ebf71a
shimeoki <shimeoki@gmail.com>
2025-08-17 21:02:18 +0300

    fix(nh): use hardcoded path
    temporarily

--- modules/nh/home.nix
+++ modules/nh/home.nix
@@ -1,12 +1,10 @@
 {
     config,
-    options,
     lib,
     ...
 }:
 let
     cfg = config.shimeoki.nh;
-    home = options.home.homeDirectory;
 in
 {
     options.shimeoki.nh = {
@@ -16,8 +14,8 @@ in
     config = lib.mkIf cfg.enable {
         programs.nh = {
             enable = true;
-            # note: flake for each user? maybe not really needed
-            flake = "${home}/nixconfig";
+            # fix: hardcoded path
+            flake = "/home/d/nixconfig";
         };
     };
 }
