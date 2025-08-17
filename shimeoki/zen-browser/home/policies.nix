1ea744f64ac8f0c5ec6e77b54fe175f44668ac62
shimeoki <shimeoki@gmail.com>
2025-08-18 02:12:44 +0300

    style: shorten some assignments

--- shimeoki/zen-browser/home/policies.nix
+++ shimeoki/zen-browser/home/policies.nix
@@ -1,23 +1,21 @@
 {
     config = {
-        programs.zen-browser = {
-            policies = {
-                AutofillAddressEnabled = true;
-                AutofillCreditCardEnabled = false;
-                DisableAppUpdate = true;
-                DisableFeedbackCommands = true;
-                DisableFirefoxStudies = true;
-                DisablePocket = true;
-                DisableTelemetry = true;
-                DontCheckDefaultBrowser = true;
-                NoDefaultBookmarks = true;
-                OfferToSaveLogins = true;
-                EnableTrackingProtection = {
-                    Value = true;
-                    Locked = true;
-                    Cryptomining = true;
-                    Fingerprinting = true;
-                };
+        programs.zen-browser.policies = {
+            AutofillAddressEnabled = true;
+            AutofillCreditCardEnabled = false;
+            DisableAppUpdate = true;
+            DisableFeedbackCommands = true;
+            DisableFirefoxStudies = true;
+            DisablePocket = true;
+            DisableTelemetry = true;
+            DontCheckDefaultBrowser = true;
+            NoDefaultBookmarks = true;
+            OfferToSaveLogins = true;
+            EnableTrackingProtection = {
+                Value = true;
+                Locked = true;
+                Cryptomining = true;
+                Fingerprinting = true;
             };
         };
     };
--- shimeoki/zen-browser/home/xdg.nix
+++ shimeoki/zen-browser/home/xdg.nix
@@ -1,25 +1,22 @@
 { config, lib, ... }:
 let
     cfg = config.shimeoki.zen-browser;
+    browser = "zen-twilight.desktop";
 in
 {
     config = lib.mkIf cfg.enable {
         xdg.mimeApps = {
             enable = lib.mkForce true;
-            defaultApplications =
-                let
-                    browser = "zen-twilight.desktop";
-                in
-                {
-                    "text/html" = browser;
-                    "text/xml" = browser;
-                    "application/xhtml+xml" = browser;
-                    "x-scheme-handler/http" = browser;
-                    "x-scheme-handler/https" = browser;
-                    "application/x-xpinstall" = browser;
-                    "application/pdf" = browser;
-                    "application/json" = browser;
-                };
+            defaultApplications = {
+                "text/html" = browser;
+                "text/xml" = browser;
+                "application/xhtml+xml" = browser;
+                "x-scheme-handler/http" = browser;
+                "x-scheme-handler/https" = browser;
+                "application/x-xpinstall" = browser;
+                "application/pdf" = browser;
+                "application/json" = browser;
+            };
         };
     };
 }
