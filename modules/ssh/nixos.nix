3085d0ab8a1813725136921bd61632a908ce5b8b
shimeoki <shimeoki@gmail.com>
2025-08-17 13:29:58 +0300

    fix(ssh): check for home-manager

--- modules/ssh/nixos.nix
+++ modules/ssh/nixos.nix
@@ -1,18 +1,19 @@
 {
     config,
+    options,
     lib,
     ...
 }:
 let
     cfg = config.shimeoki.ssh;
+    home = lib.optionalAttrs (options ? home-manager) {
+        home-manager.sharedModules = [ ./home.nix ];
+    };
 in
 {
     options.shimeoki.ssh = {
         enable = lib.mkEnableOption "ssh";
     };
 
-    config = lib.mkIf cfg.enable {
-        # todo: check for home manager
-        home-manager.sharedModules = [ ./home.nix ];
-    };
+    config = lib.mkIf cfg.enable (lib.mkMerge [ home ]);
 }
