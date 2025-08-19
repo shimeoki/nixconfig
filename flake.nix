8be2a5c59a88919c7673fa7a0ab3518b1fa884b0
shimeoki <shimeoki@gmail.com>
2025-08-20 01:24:15 +0300

    chore: flake management
    follow for every input. yes, something could break, but these project
    don't specify anything about this, so it should be fine. but the
    dependency tree, hopefully, should be less complex
    
    the flake inputs also were updated

--- flake.nix
+++ flake.nix
@@ -2,16 +2,22 @@
     description = "nixconfig";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        # maybe better not to follow?
 
-        lanzaboote = {
-            url = "github:nix-community/lanzaboote/v0.4.2";
-            inputs.nixpkgs.follows = "nixpkgs";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+
+        flake-parts.url = "github:hercules-ci/flake-parts";
+        systems.url = "github:nix-systems/x86_64-linux";
+
+        dotfiles = {
+            url = "github:shimeoki/dotfiles";
+            flake = false;
         };
 
-        niri = {
-            url = "github:/sodiboo/niri-flake";
-            inputs.nixpkgs.follows = "nixpkgs";
+        flake-utils = {
+            url = "github:numtide/flake-utils";
+            inputs.systems.follows = "systems";
         };
 
         home-manager = {
@@ -19,24 +25,56 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
-        dotfiles = {
-            url = "github:shimeoki/dotfiles";
-            flake = false;
+        lanzaboote = {
+            url = "github:nix-community/lanzaboote/v0.4.2";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+                flake-parts.follows = "flake-parts";
+                pre-commit-hooks-nix.follows = "";
+            };
+        };
+
+        niri = {
+            url = "github:sodiboo/niri-flake";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+                nixpkgs-stable.follows = "nixpkgs-stable";
+            };
+        };
+
+        nuschtosSearch = {
+            url = "github:NuschtOS/search";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+                flake-utils.follows = "flake-utils";
+            };
         };
 
         nixvim = {
             url = "github:nix-community/nixvim";
-            inputs.nixpkgs.follows = "nixpkgs";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+                flake-parts.follows = "flake-parts";
+                systems.follows = "systems";
+                nuschtosSearch.follows = "nuschtosSearch";
+            };
         };
 
         zen-browser = {
             url = "github:0xc000022070/zen-browser-flake";
-            inputs.nixpkgs.follows = "nixpkgs";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+                home-manager.follows = "home-manager";
+            };
         };
 
         stylix = {
-            url = "github:/nix-community/stylix";
-            inputs.nixpkgs.follows = "nixpkgs";
+            url = "github:nix-community/stylix";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+                flake-parts.follows = "flake-parts";
+                systems.follows = "systems";
+            };
         };
     };
 
