c861b59a2cd7d2c3cf55167fee19826e9095b1cb
shimeoki <shimeoki@gmail.com>
2025-08-22 02:31:39 +0300

    feat(niri): remaining layout options

--- shimeoki/niri/home/layout.nix
+++ shimeoki/niri/home/layout.nix
@@ -1,6 +1,10 @@
 {
     config = {
         programs.niri.settings.layout = {
+            gaps = 8;
+
+            center-focused-column = "never";
+
             default-column-width = {
                 proportion = 0.5;
             };
@@ -10,6 +14,29 @@
                 { proportion = 1.0; }
                 { proportion = 0.3; }
             ];
+
+            preset-window-heights = [
+                { proportion = 0.5; }
+                { proportion = 1.0; }
+                { proportion = 0.2; }
+            ];
+
+            tab-indicator = {
+                hide-when-single-tab = true;
+                position = "right";
+
+                gap = 0;
+                gaps-between-tabs = 0;
+                corner-radius = 0;
+
+                width = 4;
+                length.total-proportion = 1.0;
+            };
+
+            focus-ring.enable = false;
+            border.enable = false;
+
+            background-color = "transparent";
         };
     };
 }
