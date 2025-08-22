c9bd48c8eac4fb7090dd261fd5da81993f962779
shimeoki <shimeoki@gmail.com>
2025-08-23 02:38:46 +0300

    feat(nushell): set as default shell

--- shimeoki/nushell/nixos.nix
+++ shimeoki/nushell/nixos.nix
@@ -1,14 +1,21 @@
 {
     config,
     options,
+    pkgs,
     lib,
     ...
 }:
 let
     module = config.shimeoki;
     cfg = module.nushell;
+
     home = lib.optionalAttrs (options ? home-manager) {
         home-manager.sharedModules = [ ./home ];
+    };
+
+    nixos = {
+        # set as default shell. maybe add as an option?
+        users.users.d.shell = pkgs.nushell; # fix: hardcoded username
     };
 in
 {
@@ -18,5 +25,10 @@ in
         };
     };
 
-    config = lib.mkIf cfg.enable (lib.mkMerge [ home ]);
+    config = lib.mkIf cfg.enable (
+        lib.mkMerge [
+            home
+            nixos
+        ]
+    );
 }
