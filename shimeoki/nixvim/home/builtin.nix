9083bbd4eeb5b8c5fe8893a87267b81febfae1fa
shimeoki <shimeoki@gmail.com>
2025-08-24 03:05:56 +0300

    feat(nixvim): disable zip

--- shimeoki/nixvim/home/builtin.nix
+++ shimeoki/nixvim/home/builtin.nix
@@ -4,6 +4,10 @@
             # disable netrw (directory listing)
             loaded_netrw = 1;
             loaded_netrwPlugin = 1;
+
+            # disable zip (i haven't even used it)
+            loaded_zip = 1;
+            loaded_zipPlugin = 1;
         };
     };
 }
