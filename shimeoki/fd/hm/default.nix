2010c81b3858d7b2c2430bcc41eb7fa16e930da2
shimeoki <shimeoki@gmail.com>
2025-08-26 17:52:29 +0300

    refactor(fd): inherit

--- shimeoki/fd/hm/default.nix
+++ shimeoki/fd/hm/default.nix
@@ -4,17 +4,17 @@
     ...
 }:
 let
-    module = config.shimeoki;
-    cfg = module.fd;
+    inherit (config) shimeoki;
+    inherit (shimeoki) fd;
 in
 {
     options.shimeoki.fd = {
         enable = lib.mkEnableOption "fd" // {
-            default = module.enable;
+            default = shimeoki.enable;
         };
     };
 
-    config = lib.mkIf cfg.enable {
+    config = lib.mkIf fd.enable {
         programs.fd = {
             enable = true;
             hidden = true;
