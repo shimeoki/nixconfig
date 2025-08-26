e1871ee4b15da96f7bff4ddd20cc5c24d2017d8f
shimeoki <shimeoki@gmail.com>
2025-08-26 20:48:06 +0300

    style: remove outdated comment

--- shimeoki/fzf/hm/preview.nix
+++ shimeoki/fzf/hm/preview.nix
@@ -27,7 +27,6 @@ let
     changeDirWidgetOptions = opts;
 in
 {
-    # fix: check for dotfiles scripts
     config = lib.mkIf enable {
         home.packages = [ pkgs.file ]; # dependency for the fzf-preview.bash
         programs.fzf = {
