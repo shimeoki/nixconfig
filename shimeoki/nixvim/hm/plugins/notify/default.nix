884520a7be67a47c702e6f6bdb7fa2d53c527d21
shimeoki <shimeoki@gmail.com>
2025-09-17 17:14:43 +0300

    fix(nixvim): notify background color

--- shimeoki/nixvim/hm/plugins/notify/default.nix
+++ shimeoki/nixvim/hm/plugins/notify/default.nix
@@ -17,7 +17,14 @@ in
     config = lib.mkIf cfg.enable {
         programs.nixvim.plugins.notify = {
             enable = true;
-            settings.fps = 60; # fix: author specific
+            settings = {
+                fps = 60; # FIX: author specific
+
+                # NOTE: probably should only be used if the colors are
+                # configured to be transparent. not tested. included to just
+                # get rid of the annoying warning notification.
+                background_colour = "#000000";
+            };
         };
     };
 }
