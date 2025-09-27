edd3f0fd4732a25d6781393e0848e39b732f32c7
shimeoki <shimeoki@gmail.com>
2025-09-27 16:48:53 +0300

    feat(nixvim): tsls setup

--- shimeoki/nixvim/hm/languages/web/lsp.nix
+++ shimeoki/nixvim/hm/languages/web/lsp.nix
@@ -7,7 +7,28 @@ in
     config = lib.mkIf (cfg.enable && lsp.enable) {
         programs.nixvim.lsp.servers = {
             cssls.enable = true;
-            # TODO: tsls setup
+
+            ts_ls = {
+                enable = true;
+                settings = {
+                    workspace_required = true;
+                    filetypes = [
+                        "javascript"
+                        "javascriptreact"
+                        "typescript"
+                        "typescriptreact"
+                        "vue" # TODO: vue setup
+                    ];
+                    root_markers = [
+                        "package-lock.json"
+                        "yarn.lock"
+                        "pnpm-lock.yaml"
+                        "bun.lockb"
+                        "bun.lock"
+                    ];
+                };
+            };
+
             # TODO: denols setup
             # TODO: svelte setup
         };
