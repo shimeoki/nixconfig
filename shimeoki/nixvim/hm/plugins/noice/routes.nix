7833979523ee7ada7c067c79fa9c550f30dcf02b
shimeoki <shimeoki@gmail.com>
2025-10-14 10:26:37 +0300

    feat(nixvim): skip jdtls progress

--- shimeoki/nixvim/hm/plugins/noice/routes.nix
+++ shimeoki/nixvim/hm/plugins/noice/routes.nix
@@ -18,6 +18,35 @@
                     find = "written";
                 };
             }
+            {
+                # note: hide jdtls progress
+                opts.skip = true;
+                filter = {
+                    event = "lsp";
+                    kind = "progress";
+                    cond.__raw = ''
+                        function(m)
+                            if not m.opts or not m.opts.progress then
+                                return false
+                            end
+
+                            local msg = m.opts.progress
+
+                            if msg.client ~= "jdtls" then
+                                return false
+                            end
+
+                            local jdtls_progress = {
+                                ["Building"] = true,
+                                ["Validate documents"] = true,
+                                ["Publish Diagnostics"] = true,
+                            }
+
+                            return jdtls_progress[msg.message] or false
+                        end
+                    '';
+                };
+            }
         ];
     };
 }
