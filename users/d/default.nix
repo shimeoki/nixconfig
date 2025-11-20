4bda0ddfd58b9716ba6685fb1d0d71ffadff2fb1
shimeoki <shimeoki@gmail.com>
2025-11-20 18:28:02 +0300

    feat(users/d): add fonts
    needed for rmpc and just in general

--- users/d/default.nix
+++ users/d/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 let
     name = "d";
 in
@@ -19,6 +19,21 @@ in
         nushell.users = [ name ];
         greetd.user = name;
     };
+
+    fonts.packages = with pkgs; [
+        # keep-sorted start
+        fira-code
+        nerd-fonts.fira-code
+        nerd-fonts.noto
+        nerd-fonts.roboto-mono
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-color-emoji
+        noto-fonts-lgc-plus
+        roboto
+        roboto-mono
+        # keep-sorted end
+    ];
 
     home-manager.users."${name}" = ./hm;
 }
